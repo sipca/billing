4d53203cc707d789eb6b1784a99223256ee160c0
vov1zzz <vn.nvaavn@gmail.com>
2025-09-09 22:28:52 +0200

    fix

--- backend/views/call/index.php
+++ backend/views/call/index.php
@@ -58,7 +58,8 @@ $this->params['breadcrumbs'][] = $this->title;
                 "attribute" => "billing_duration",
                 "value" => function(\common\models\Call $model) {
                     $profit = $model->getSum() - $model->getSumSupplier();
-                    return Yii::$app->formatter->asDuration($model->billing_duration) . " (" . Yii::$app->formatter->asCurrency($model->getSum()) ." | ".Yii::$app->formatter->asCurrency($profit) .")";
+                    $duration_text = $model->billing_duration ? Yii::$app->formatter->asDuration($model->billing_duration) : "";
+                    return $duration_text . " (" . Yii::$app->formatter->asCurrency($model->getSum()) ." | ".Yii::$app->formatter->asCurrency($profit) .")";
                 },
                 'pageSummary'=> Yii::$app->formatter->asCurrency($totalSum) ." | " . Yii::$app->formatter->asCurrency($totalProfit),
             ],
